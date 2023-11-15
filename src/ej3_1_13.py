2403ce95580332892c5a0212d26571ab76bcadb7
ivanjim13 <ivangorrion57@gmail.com>
2023-11-15 23:11:57 +0100

    p3_1 terminado

--- src/ej3_1_13.py
+++ src/ej3_1_13.py
@@ -1,20 +1,30 @@
 # Escribir un programa que pregunte por una muestra de números, separados por comas, los guarde en una lista y muestre por pantalla su media y desviación típica.
 from borrarPantalla import borrarPantalla
+import statistics
 
 def pedirNumeros():
     return input("Introduce números separados por coma: ")
     
 
 
-def lista(num):
+def listas(num):
     lista = num.split(",")
-    print(lista)
+    lista2 = [int(i) for i in lista]
+    print(lista2)
+    media_y_Desviacion(lista2)
 
+
+def media_y_Desviacion(lista2):
+    media = statistics.mean(lista2)
+    desviacion = statistics.stdev(lista2)
+    print(f"La media es de: {round(media,2)}")
+    print(f"La desviación es de: {round(desviacion,2)}")
 
 def main():
     borrarPantalla()
     num = pedirNumeros()
-    lista(num)
+    listas(num)
+    
 
 
 if __name__ == "__main__":
