e7ada17343c65ac153b4b31d015c32031e53139d
ilopjim13 <ljivan@iesrafaelalberti.es>
2023-11-15 19:46:02 +0100

    desde clase

--- src/ej3_1_13.py
+++ src/ej3_1_13.py
@@ -1,1 +1,21 @@
 # Escribir un programa que pregunte por una muestra de números, separados por comas, los guarde en una lista y muestre por pantalla su media y desviación típica.
+from borrarPantalla import borrarPantalla
+
+def pedirNumeros():
+    return input("Introduce números separados por coma: ")
+    
+
+
+def lista(num):
+    lista = num.split(",")
+    print(lista)
+
+
+def main():
+    borrarPantalla()
+    num = pedirNumeros()
+    lista(num)
+
+
+if __name__ == "__main__":
+    main()
